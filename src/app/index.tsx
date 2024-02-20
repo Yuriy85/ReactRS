a55477851fe167876781ae0e35e03b8bd2ae1d36
Yuriy85 <yurik8585@gmail.com>
2024-02-20 12:40:29 +0300

    feat: add search

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,42 @@
-function App() {
-  console.log(1);
-  return <h1>ReactRS</h1>;
+import { Component, ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  age: number;
+}
+
+class App extends Component<Props, State> {
+  public state: State = {
+    age: 1,
+  };
+  componentDidMount() {
+    console.log(1);
+  }
+  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>): void {
+    console.log(prevProps, prevState);
+  }
+  componentWillUnmount() {
+    console.log(2);
+  }
+  render() {
+    return (
+      <>
+        <header></header>
+        <main>
+          <h1>ReactRS</h1>
+          <div className="search container">
+            <input className="search__input input"></input>
+            <button className="search__button button">Search</button>
+          </div>
+          <div className="data container"></div>
+        </main>
+        <footer></footer>
+      </>
+    );
+  }
 }
 
 export default App;
